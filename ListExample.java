1fbae1d546bce0412a8904fb38b0e1c9ad310ece
Jyoti Prakash Behera <bjyotiprakash04@gmail.com>
2025-11-03 19:12:08 +0530

    Add files via upload

---
+++ ListExample.java
@@ -0,0 +1,11 @@
+import java.util.*; // Import the List and ArrayList classes
+
+public class ListExample {
+    public static void main(String[] args) {
+        List<String> names = new ArrayList<>();
+        names.add("Jyoti");
+        names.add("Prakash");
+        names.add("Behera");
+        System.out.println("List" + names);
+    }
+}
